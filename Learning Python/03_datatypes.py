11b52d6165f8ff42d9baa0fc07508375e48478fe
“Nandanvar <nandanwarkrish86@gmail.com>
2026-06-10 20:19:29 +0530

    Update 03_datatypes.py

--- Learning Python/03_datatypes.py
+++ Learning Python/03_datatypes.py
@@ -1,12 +1,21 @@
+# STRING
 name = "Krish"
 print(type(name))
 
+#INTEGER
 age = 18
 print(type(age))
 
+# FLOAT
 decimal = 0.69
 print(type(decimal))
 
+#BOOLEAN
 is_student = True
 print(type(is_student))
+
+# COMPLEX NUMBERS
+
+img = 3 + 4j # using 'j' is compulsary to create a complex number!
+print(type(img))
 
