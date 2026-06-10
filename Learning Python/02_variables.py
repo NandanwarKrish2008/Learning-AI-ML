b449c31d3db5a63e0131a349ca5a66db99a43a6c
“Nandanvar <nandanwarkrish86@gmail.com>
2026-06-10 20:09:55 +0530

    Update 02_variables.py

--- Learning Python/02_variables.py
+++ Learning Python/02_variables.py
@@ -1,2 +1,20 @@
-name = "Krish"
+name = "krish"
 age = 18
+city = "surat"
+is_student = True
+
+
+# NAMING CONCVENTIONS
+
+
+#camelCase
+
+myName = "krish"
+
+# PascalCase
+
+MyName = "krish"
+
+# snake_case
+
+my_name = "krish"
