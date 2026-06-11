002d749266375c2df5d0cd7938601e16d655cea1
“Nandanvar <nandanwarkrish86@gmail.com>
2026-06-11 15:44:46 +0530

    Update 04_string.py

--- Learning Python/04_string.py
+++ Learning Python/04_string.py
@@ -1,8 +1,13 @@
-char1 = "k"
-print(ord(char1)) # Prints the unicode of the character
+# char1 = "k"
+# print(ord(char1)) # Prints the unicode of the character
 
-char2 = "K"
-print(ord(char2))
+# char2 = "K"
+# print(ord(char2))
 
-char3 = " "
-print(ord(char3))
+# char3 = " "
+# print(ord(char3))
+
+name = "krish"
+
+print(name)
+print(name[0])
