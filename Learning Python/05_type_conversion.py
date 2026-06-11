691edc0f0811734f587944b8f93692aef1cf08b8
“Nandanvar <nandanwarkrish86@gmail.com>
2026-06-11 15:59:56 +0530

    Update 05_type_conversion.py

--- Learning Python/05_type_conversion.py
+++ Learning Python/05_type_conversion.py
@@ -1,7 +1,14 @@
-a = "12"
-print(type(a))
-print(a)
+# a = "12"
+# print(type(a))
+# print(a)
 
-b = int(a)
-print(type(b))
-print(b)
+# b = int(a)
+# print(type(b))
+# print(b)
+
+c = "12.3"
+print(type(c))
+
+c = float(c)
+print(type(c))
+
